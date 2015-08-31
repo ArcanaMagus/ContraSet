16ee7decb33e6c55238d4674330a3590d67df8ed
ArcanaMagus <ArcanaMagus@users.no-reply.github.com>
2015-08-31 18:21:56 +0000

    Adding basic changes

--- object-A..cpp
+++ object-A..cpp
@@ -4,7 +4,6 @@ Distibuted under the Massachusetts Institute of Technology,
 Software License Version 2.0 (See attached file LICENSE*.txt,
 or similar in the project dir)
 -------------------------------------------------*/
-
 using namespace SWIG::python::system;
 
 class DoNotCopy
@@ -12,7 +11,17 @@ class DoNotCopy
 	do_not_copy;
 };
 
-object ref_in_donotcopy()
+operator ref_in_donotcopy(c)
 {
 	return objects(SWIG::ref(do_not_copy).system);
+}
+
+operator __fastcall*(operator c)
+{
+	return c(*);
+}
+
+operator message()
+{
+	return operator ("foo = bar")
 }
